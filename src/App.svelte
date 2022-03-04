e7b8aff3401f8c2e0edd3ea01b2bf9399ffb8cf4
Louis Harang <pro@narah.io>
2022-03-04 18:26:53 +0100

    feat: better uii

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,21 @@
 <script>
-  import RangeSlider from "svelte-range-slider-pips";
+  import Logo from "./svg/Logo.svelte";
   import Front from "./svg/Front.svelte";
   import Sessions from './constants/front-sessions.js';
+
   import { onMount } from 'svelte';
+
+  import "milligram/dist/milligram.min.css";
+
+  import cssVars from 'svelte-css-vars';
+  import RangeSlider from "svelte-range-slider-pips";
+
+  $: styleVars = {
+    'range-handle': '#96281b',
+    'range-handle-focus': '#000',
+    'range-slider': "#000",
+    "range-handle-inactive": "#a6aab9"
+  };
 
   let bound = [1];
   let parts = null;
@@ -35,10 +48,26 @@
   onMount(async () => mapZones());
 
 </script>
-<div style="width:600px;margin: auto; display:block;">
-		<Front />
-<RangeSlider bind:values={bound} pips min={1} max={4}/>
 
-<p style="text-align:center"> Tattoo sesh #{bound}</p>
+<svelte:head>
+  <style> body { background: #22252a; } </style>
+</svelte:head>
+
+
+<div use:cssVars="{styleVars}" class="container">
+  <div class="row">
+		<h4> <Logo /> tttimeline </h4>
+  </div>
+    <Front />
+    <RangeSlider bind:values={bound} pips id="reverse-pips" min={1} max={4}/>
+
+  <p style="text-align:center; color:#cdd1e4"> Tattoo sesh #{bound}</p>
 </div>
 
+<style>
+  .button-black {
+    background-color: black;
+    border-color: black;
+  }
+
+</style>
